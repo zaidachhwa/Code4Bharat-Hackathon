556d0f2212f8ea846422406182b562d9a24e0fb7
zaidachhwa <zaidachhwa8486@gmail.com>
2025-12-12 09:07:12 +0530

    configured next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export",
   // assetPrefix:
   //   "https://69143d625d5adc67a990d843.storage.fermion.app/public-cached/custom-page-zip-contents.",
 };
