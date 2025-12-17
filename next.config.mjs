7921f6d4adddaa31e58ed6012e7b6eb688f1f3eb
noorulhaqueshaikh <noorulhaque560@gmail.com>
2025-12-17 18:19:35 +0530

    cahnge

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: "export",
-  assetPrefix: "https://69143d625d5adc67a990d843.storage.fermion.app/public-cached/custom-page-zip-contents",
+  output: "export",
+  assetPrefix:
+    "https://69143d625d5adc67a990d843.storage.fermion.app/public-cached/custom-page-zip-contents",
   images: {
     unoptimized: true,
   },
