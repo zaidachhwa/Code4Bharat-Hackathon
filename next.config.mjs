ccfc2f1ca8f1c311a5747d0b54cd6e59972434d9
noorulhaqueshaikh <noorulhaque560@gmail.com>
2025-12-18 10:33:01 +0530

    correction for amb details page

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix:
-    "https://69143d625d5adc67a990d843.storage.fermion.app/public-cached/custom-page-zip-contents",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // assetPrefix:
+  //   "https://69143d625d5adc67a990d843.storage.fermion.app/public-cached/custom-page-zip-contents",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
